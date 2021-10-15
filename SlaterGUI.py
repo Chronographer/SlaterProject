7c4fe9762416df73155de7dc0477228e18cfdb1f
Daniel <djisenberg2015@gmail.com>
2021-10-15 12:39:12 -0400

    Added ability to plot multiple orbital configurations simultaneously.

--- SlaterGUI.py
+++ SlaterGUI.py
@@ -17,9 +17,11 @@ while 1:
 
     listLength = integerbox("Enter (integer) length of grid.", "Grid Length")
 
-    choices1 = ["Exponential", "Uniform"]
+    exponentialOrUniform = ["Exponential", "Uniform"]
 
-    scaleType = buttonbox("Would you like an exponential or uniform grid?", "Grid Type", choices1)
+    scaleType = buttonbox("Would you like an exponential or uniform grid? (They produce identical plots for now)", "Grid Type", exponentialOrUniform)
+
+    independentOrbitalNumber = integerbox("How many orbitals would you like to plot independently?")
 
     if scaleType == "Exponential":
         arrayx = Routines.ExpGridStretch2(numpy.arange(0.01, 1.0 * listLength, 0.01))
@@ -29,17 +31,25 @@ while 1:
     subshell = ["1s", "2s&p", "3s&p", "3d", "4s&p", "4d", "4f", "5s&p", "5d"]
 
     occupancy = []
-
-    for i in range(9):
-        occupancy.append(integerbox("Enter the occupancy of the " + subshell[i] + " subshell.", "Subshell Selection"))
-    dty = 4 * numpy.pi * arrayx**2 * Slater.density(arrayx, Z, occupancy)[0]  # what is 'dty'? Density?
-    xList = []
-    yList = []
-    for i in range(len(arrayx)):
-        xList.append(arrayx[i])
-        yList.append(dty[i])
-    plt.plot(xList, yList)
-    plt.title("Plot of <something> vs. <radius> for atomic number " + str(Z))
+    xListMaster = []
+    yListMaster = []
+    for orbital in range(independentOrbitalNumber):
+        for i in range(9):
+            occupancy.append(integerbox("Enter the occupancy of the " + subshell[i] + " subshell.", "Subshell Selection"))
+        dty = 4 * numpy.pi * arrayx**2 * Slater.density(arrayx, Z, occupancy)[0]
+        xList = []
+        yList = []
+        for i in range(len(arrayx)):
+            xList.append(arrayx[i])
+            yList.append(dty[i])
+        xListMaster.append(xList)
+        yListMaster.append(yList)
+        occupancy.clear()
+        #xList.clear()
+        #yList.clear()
+    for plotNumber in range(len(xListMaster)):
+        plt.plot(xListMaster[plotNumber], yListMaster[plotNumber])
+    plt.title("Plot of <something> vs. radius for atomic number " + str(Z))
     plt.xlabel("radius")
     plt.ylabel("4pi r ^2")
     plt.show()
