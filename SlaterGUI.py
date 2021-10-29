e18c0cc265b6dff1eac98c55d1719eb9330114f3
Daniel <djisenberg2015@gmail.com>
2021-10-29 12:10:03 -0400

    Added ability to omit input of trailing zero's by having the program add them to the end of the input list if there are less than 9 elements. Added a check that prevents you from supplying an input string with more than 9 orbital occupancies. Added mechanism to remove the last element of the string if it is not a digit before it is converted from a string to a list of numbers, thus preventing a runtime error. The idea is that the user might accidentally add an extra '.' at the end, in which case removing it would result in the intended input string; If instead they failed to enter a non-zero trailing digit, this will result in the wrong system being modeled. Because of this, a terminal print statement has been added explaining this.

--- SlaterGUI.py
+++ SlaterGUI.py
@@ -21,7 +21,7 @@ while 1:
 
     scaleType = buttonbox("Would you like an exponential or uniform grid? (They produce identical plots for now)", "Grid Type", exponentialOrUniform)
 
-    independentOrbitalNumber = integerbox("How many orbitals would you like to plot independently?")
+
 
     """legendLabelList = [] # for eventual automatic creation of legend labels.
     for legendIndex in range(independentOrbitalNumber):
@@ -36,14 +36,24 @@ while 1:
 
     subshell = ["1s", "2s&p", "3s&p", "3d", "4s&p", "4d", "4f", "5s&p", "5d"]
 
-    occupancy = []
     xListMaster = []
     yListMaster = []
-    orbitalConfig = enterbox("Enter the orbital configuration of each shell as a period separated list.")
-    orbitalConfigList = orbitalConfig.split(".")
+    orbitalConfigList = []
+    while len(orbitalConfigList) != 9:
+        orbitalConfigString = enterbox("Enter the orbital configuration of each shell as a period separated list.\n\nThere should be no more than 9 elements.\n\nTrailing zero's can be omitted and will be automatically added to the end of the list as needed.")
+        if not orbitalConfigString[len(orbitalConfigString)-1].isdigit():
+            print("\nWARNING: the last character of your input string was '" + orbitalConfigString[len(orbitalConfigString)-1] + "', which is not a digit. You might have accidentally left out a number or entered it incorrectly. The input you provided was: '" + orbitalConfigString + "'\nThe anomalous character has been removed in an attempt to prevent a runtime error.\nBe cautious; even if a runtime error is not thrown, your results might not reflect the system you intended to model!\n")
+            orbitalConfigString = orbitalConfigString[:-1]  # this removes the last character from the input string; the idea is that if you accidentally end with a '.', this will remove it for you. Will produce incorrect results if the user left out a non-zero number after the last character.
+        orbitalConfigList = orbitalConfigString.split(".")
+        if len(orbitalConfigList) > 9:
+            msgbox("There are " + str(len(orbitalConfigList)) + " elements in orbitalConfigList, there should be no more than 9.\nRe-enter the orbital configuration of each shell as a period separated list.")
+        elif len(orbitalConfigList) < 9:
+            while len(orbitalConfigList) < 9:  # Allows user to omit all trailing 0's by adding as many as are needed to generate a list of correct length, thus saving time.
+                orbitalConfigList.append(0)
     for element in range(0, len(orbitalConfigList)):
         orbitalConfigList[element] = int(orbitalConfigList[element])
-    dty = 4 * numpy.pi * arrayx**2 * Slater.density(arrayx, Z, occupancy)[0]
+
+    dty = 4 * numpy.pi * arrayx**2 * Slater.density(arrayx, Z, orbitalConfigList)[0]
     xList = []
     yList = []
     for i in range(len(arrayx)):
@@ -51,7 +61,6 @@ while 1:
         yList.append(dty[i])
     xListMaster.append(xList)
     yListMaster.append(yList)
-    occupancy.clear()
     for plotNumber in range(len(xListMaster)):
         plt.plot(xListMaster[plotNumber], yListMaster[plotNumber])
     plt.title("Plot of <something> vs. radius for atomic number " + str(Z))
