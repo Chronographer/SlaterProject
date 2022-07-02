45f01fa5a47f82cacbe99282936e167f9db54fa1
Daniel <djisenberg2015@gmail.com>
2022-07-02 19:22:32 -0400

    Added a preliminary setup.py file (using PyCharm) to eventually publish to PyPI (I think the IDE has tools that let you do this very easily, but I will have to experiment more to be sure)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SlaterProject',
+    version='0.9.0',
+    packages=[''],
+    url='https://github.com/Chronographer/SlaterProject',
+    license='GNU GPLv3',
+    author='Daniel',
+    author_email='djisenberg@bsu.edu',
+    description='Robust tool for computing Slater electron densities'
+)
